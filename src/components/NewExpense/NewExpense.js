f009c15b7a2bf86b54afc8a9862c492ca87c76a9
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-08-29 12:17:27 +0300

    feat: add hiding form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
 import ExpenseForm  from './ExpenseForm';
+import { useState } from 'react';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setEditing] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredData) => {
     const expenseData = {
       ...enteredData,
@@ -9,11 +12,30 @@ const NewExpense = (props) => {
     }
 
     props.onAddExpense(expenseData);
+    setEditing(false);
+  }
+
+  const startEditingHandler = () => {
+    setEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setEditing(false);
+  }
+
+  let newExpenseContent = (
+    <button type='button' onClick = {startEditingHandler}>Add new expense</button>
+  );
+
+  if (isEditing) {
+    newExpenseContent = <ExpenseForm 
+    onCancel={stopEditingHandler}
+    onSaveExpenseData={onSaveExpenseDataHandler}/>
   }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
+      {newExpenseContent}
     </div>
   );
 };
