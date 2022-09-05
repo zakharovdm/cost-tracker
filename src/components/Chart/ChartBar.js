33f76096fa9c77bfc2e476e817d0b0591649fcd1
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-05 18:04:02 +0300

    feat: add ChartBar component

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,23 @@
+import "./chartBar.css";
+
+const chartBar = (props) => {
+  let fillingHeight = "0%";
+
+  if (props.max > 0) {
+    fillingHeight = `${Math.round((props.value / props.maxValue) * 100)}%`;
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: fillingHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default chartBar;
