b61e41586a1bd73f06abb306ed5b9e374914d3fb
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-09-05 17:33:10 +0300

    feat: add Char component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={null}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Chart;
